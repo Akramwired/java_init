b238c9b7c11fe29fd0e56c8f3b3defadd5ddf45c
Akramwired <kahd.unplugged@gmail.com>
2020-10-24 19:04:48 +0600

    start expressions

--- src/Hello.java
+++ src/Hello.java
@@ -2,6 +2,11 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello Akram");
         int myNum = 5;
-        System.out.println(myNum);
+        int mySecondNum = 12;
+        int myThirdNum = 6;
+        int myTotal = myNum + mySecondNum + myThirdNum;
+        System.out.println(myTotal);
+        int myLastOne = 1000 - myTotal;
+        System.out.println(myLastOne);
     }
 }
